f746fdac3ee7c2edec3a81ac590aa44963a732a7
gonzalo-mg <gonzalomartinezgonzalez@gmail.com>
2022-11-14 21:00:10 +0100

    fin primer dia

--- script.js
+++ script.js
@@ -31,20 +31,61 @@ async function getPokemonList(api) {
         const apiData = await getApiData(api);
 
         // extraer de los datos de la api el array "results" y desechar el resto
-        const {results} = apiData;
-        return results
+        const {PromiseResult} = apiData;
+        return PromiseResult
     } catch (error) {
         console.error(error);
     }
 }
 
-getPokemonList(urlApiLista);
+const lista = getPokemonList(urlApiLista);
 //test
 console.log(getPokemonList(urlApiLista));
+console.log(lista);
 
 // seleccion de elementos del html
+const form = document.querySelector("#formulario");
 const input = document.querySelector("#buscador-pokemon");
 const boton = document.querySelector("#boton");
-const form = document.querySelector("#formulario");
 const articulo = document.querySelector("#articulo");
 
+// formDATA
+function formSubmitHandle(e) {
+    // Evitar recarga de pagina (comportamiento por defecto de evento submit)
+    e.preventDefault();
+        // test
+    console.log('formulario enviado');
+  
+    // Crear objeto clase FormData con los datos de los input cuando envia formulario
+    const data = new FormData(form);
+  
+    // Recorremos data para mostrar cada uno de sus valores
+    for (const pokemon of data) {
+      // elemento es un array, lo podemos destructurar
+      const [nameInput, valueInput] = pokemon;
+      console.log(valueInput);
+  
+        // Para el input nombre mostramos un console.log distinto
+    if (nameInput === 'pokedex') {
+        console.log(`El valor del input de nombre es ${valueInput}`);
+      }
+      // si el valueInput existe en el array results hacer nuevo fetch
+      for (element of PromiseResult) {
+        if (valueInput === results.name) {
+            console.log("El pokemon buscado no existe´");
+          }
+        else {
+            console.log(`El pokemon buscado existe y es ${results.name}`);
+          }
+      }
+    
+    }
+  
+    
+
+    // Al final limpiar el formulario
+    form.reset();
+}
+
+  // Asignamos al formulario esta funcion manejadora del evento de submit
+  form.addEventListener('submit', formSubmitHandle);
